18def00786ba982c7848b7d2f17672e09261f41e
toroscopo <torudiel@gmail.com>
2018-06-13 16:57:29 +0200

    Añadido m_sing_in
                                                                                             Este doc tiene una función que recibe un id de clase y asigna a los alumnos a dicha clase

--- codeigniter/application/models/m_signed_in.php
+++ codeigniter/application/models/m_signed_in.php
@@ -30,6 +30,8 @@ class m_signed_in extends CI_Model
         $this -> db -> from('USER');
         $this -> db -> where('grade', $grade);
 
+        $query = $this->db->get();
+        //Esta es la parte que quizá no funcione
         foreach ($query->result() as $student)
         {
             $insert  = array(
@@ -37,6 +39,29 @@ class m_signed_in extends CI_Model
                 'grade' => $grade,
             );
             $this->db->insert('signed_in', $insert);
+        }
+
+        $this->db->trans_complete();
+
+    }
+
+
+    function sign_student_group($id_group, $id_class){
+
+        $this->db->trans_start();
+
+
+        $this -> db -> select('id_student');
+        $this -> db -> from('SIGNED_IN');
+        $this -> db -> where('id_class', $id_class);
+
+        $query = $this->db->get();
+
+        foreach ($query->result() as $row)
+        {
+            $this->db->set('id_group');
+            $this->db->where('id_student', $row);
+            $this->db->update('SIGNED_IN');
         }
 
     }
